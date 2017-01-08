9aa03bc94cd0a761bdf27b2c7423bb813da242a3
Martijn van Put <martijn@vanput.net>
2017-01-08 20:43:49 +0100

    Created aliases for database drop and database update and base tooling for the dotnet ef cli.

---
+++ src/Cake.DotNetCoreEf/Database/DotNetCoreEfDatabaseDropSettings.cs
@@ -0,0 +1,25 @@
+﻿using Cake.Common.Tools.DotNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake.DotNetCoreEf.Database
+{
+    /// <summary>
+    /// Contains settings used by <see cref="DotNetCoreEfDatabaseDropper"/>
+    /// </summary>
+    public class DotNetCoreEfDatabaseDropSettings : DotNetCoreEfSettings
+    {       
+        /// <summary>
+        /// The DbContext to use. If omitted, the default DbContext is used
+        /// </summary>
+        public string Context { get; set; }
+
+        /// <summary>
+        /// Drop without confirmation
+        /// </summary>
+        public bool Force { get; set; }
+    }
+}
---
+++ src/Cake.DotNetCoreEf/Database/DotNetCoreEfDatabaseDropper.cs
@@ -0,0 +1,85 @@
+﻿using Cake.Common.Tools.DotNetCore;
+using Cake.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.DotNetCoreEf.Database
+{
+    /// <summary>
+    /// Support for dropping the database using .NET Core cli tooling
+    /// </summary>
+    public class DotNetCoreEfDatabaseDropper : DotNetCoreEfTool<DotNetCoreEfDatabaseDropSettings>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DotNetCoreEfDatabaseDropper" /> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        /// <param name="tools">The tool locator.</param>
+        public DotNetCoreEfDatabaseDropper(
+            IFileSystem fileSystem, 
+            ICakeEnvironment environment, 
+            IProcessRunner processRunner, 
+            IToolLocator tools) : base(fileSystem, environment, processRunner, tools)
+        {
+        }
+
+        /// <summary>
+        /// Runs the project using the specified path with arguments and settings.
+        /// </summary>
+        /// <param name="project">The target project path.</param>
+        /// <param name="arguments">The arguments.</param>
+        /// <param name="settings">The settings.</param>
+        public void Run(string project, ProcessArgumentBuilder arguments, DotNetCoreEfDatabaseDropSettings settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            Run(settings, GetArguments(project, arguments, settings));
+        }
+
+        private ProcessArgumentBuilder GetArguments(string project, ProcessArgumentBuilder arguments, DotNetCoreEfDatabaseDropSettings settings)
+        {
+            var builder = CreateArgumentBuilder(settings);
+
+            builder.Append("ef");
+            builder.Append("database");
+            builder.Append("drop");
+
+            // Specific path?
+            if (project != null)
+            {
+                builder.Append("--project");
+                builder.AppendQuoted(project);
+            }           
+
+            if (!string.IsNullOrEmpty(settings.Context))
+            {
+                builder.Append("--context");
+                builder.AppendQuoted(settings.Context);
+            }
+
+            if (settings.Force)
+            {
+                builder.Append("--force");
+            }           
+
+            // Arguments
+            if (!arguments.IsNullOrEmpty())
+            {
+                builder.Append("--");
+                arguments.CopyTo(builder);
+            }
+
+            return builder;
+        }
+    }
+}

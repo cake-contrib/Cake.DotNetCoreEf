1114dc6af75a14d0b3af907ad20c1702da0776d3
Martijn van Put <martijn@vanput.net>
2024-01-21 10:46:45 +0100

    Update to latest version (#55)

--- src/Cake.DotNetCoreEf/Database/DotNetCoreEfDatabaseDropSettings.cs
+++ src/Cake.DotNetCoreEf/Database/DotNetCoreEfDatabaseDropSettings.cs
@@ -1,11 +1,4 @@
-﻿using Cake.Common.Tools.DotNetCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cake.DotNetCoreEf.Database
+﻿namespace Cake.DotNetCoreEf.Database
 {
     /// <summary>
     /// Contains settings used by <see cref="DotNetCoreEfDatabaseDropper"/>
--- src/Cake.DotNetCoreEf/ProcessArgumentBuilderExtensions.cs
+++ src/Cake.DotNetCoreEf/ProcessArgumentBuilderExtensions.cs
@@ -1,10 +1,5 @@
 ﻿using Cake.Core;
 using Cake.Core.IO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cake.DotNetCoreEf
 {
